7fd86986520730ed1bb1d1041ab74e7dffec0cb7
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-05-06 22:41:27 +0300

    Using an index value outside the range of existing elements using get

--- src/myvec.rs
+++ src/myvec.rs
@@ -8,10 +8,11 @@ pub fn test_vec_int(){
     println!("{:?}", my_ints);
     println!("The length of Vec: {:?}", my_ints.len());
     println!("The capacity of Vec: {:?}", my_ints.capacity());
-    println!("My first born brother is {:?} years old", my_ints[2]);
+    println!("My first born brother is {:?} years old", &my_ints[2]);
 
     let v = vec![10,20,30,40,50,60,70,80,90];
-    println!("{:?}When rounded of to the nearest hundreds they become 100", &v.as_slice()[4..])
+    println!("{:?}When rounded of to the nearest hundreds they become 100", &v.as_slice()[4..]);
+    println!("Testing the get method of referencing a value {:?}", v.get(10));
 
     
 }
