cb037e4beb416012cd45e0898a050a8de1e406de
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-05-06 20:29:32 +0300

    Length v Capacity of vector comparrison

--- src/myvec.rs
+++ src/myvec.rs
@@ -2,5 +2,11 @@ pub fn test_vec_int(){
     let mut my_ints: Vec<i32> = Vec::new();
 
     my_ints.push(25);
+    my_ints.push(27);
+    my_ints.push(29);
+
+
     println!("{:?}", my_ints);
+    println!("The length of Vec: {:?}", my_ints.len());
+    println!("The capacity of Vec: {:?}", my_ints.capacity());
 }
