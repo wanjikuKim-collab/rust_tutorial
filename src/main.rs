55b78d6aa0b7955cb179678a545f5a3f220f6333
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-05-09 15:56:59 +0300

    Testing out btop

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,7 @@ fn main() {
     myvec::test_vec_string();
     myvec::manufacture_cars();
     myvec::merge_company();
+
+    let mut input: String = "".to_string();
+    std::io::stdin().read_line(&mut input).expect("Something wrong happened");
 }
